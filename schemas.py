0c6fb80ba24d59d66a5af0474e8a2a7af242dcec
Kenji <silasotsuka@gmail.com>
2025-04-08 09:47:36 -0300

    Feat: adicionando remodulacao com alembit

--- schemas.py
+++ schemas.py
@@ -3,20 +3,37 @@ from typing import Optional
 from datetime import date
 
 class DispositivoBase(BaseModel):
-    id_tomb: int
     tipo_de_disp: str
+    qnt_ram: Optional[int]
+    qnt_armaz: Optional[int]
+    tipo_armaz: Optional[str]
     marca: str
-    qnt_ram: Optional[int] = None
-    qnt_armaz: Optional[int] = None
-    tipo_armaz: Optional[str] = None
-    funcionando: Optional[bool] = None
-    locat_do_disp: Optional[str] = None
-    descricao: Optional[str] = None
-    data_de_an: Optional[date] = None
+    modelo: str
+    funcionando: Optional[bool]
+    data_de_an: Optional[date]
+    locat_do_disp: Optional[str]
+    descricao: Optional[str]
 
 class DispositivoCreate(DispositivoBase):
     pass
 
-class Dispositivo(DispositivoBase):
+class DispositivoUpdate(DispositivoBase):
+    pass
+
+class DispositivoOut(DispositivoBase):
+    id_tomb: int
+
     class Config:
-        from_attributes = True
+        orm_mode = True
+
+class LogAtualizacaoOut(BaseModel):
+    id_log: int
+    id_tomb: int
+    campo_alterado: str
+    valor_antigo: Optional[str]
+    valor_novo: Optional[str]
+    data_hora_alteracao: date
+
+
+    class Config:
+        orm_mode = True
